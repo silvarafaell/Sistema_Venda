c04f7c317555da7dde3f0ff4144faa40f3f322d3
francisco rafael <francisco_rafael@hotmail.com.br>
2023-02-28 19:47:06 -0300

    cors

--- SistemaVenda/SistemaVenda.API/Program.cs
+++ SistemaVenda/SistemaVenda.API/Program.cs
@@ -16,6 +16,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors();
 
 var app = builder.Build();
 
@@ -25,6 +26,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors(x => x.AllowAnyHeader()
+                              .AllowAnyMethod()
+                              .AllowAnyOrigin());
 
 app.UseHttpsRedirection();
 
